2f649cfa2967b7530358e351de3adabe464fa41f
Eddie Cameron <eddiec@grapefruitgames.com>
2012-02-28 13:01:52 -0500

    Fixed namespace

--- Main.cs
+++ Main.cs
@@ -1,34 +1,32 @@
 using System;
-
-namespace Streamer
+using Streamer;
+	
+class MainClass
 {
-	class MainClass
+	public static void Main ( string[] args )
 	{
-		public static void Main ( string[] args )
-		{
-			// Create twitteraccess object
-			TwitterAccess access = new TwitterAccess();
+		// Create twitteraccess object
+		TwitterAccess access = new TwitterAccess();
+	
+		// Get all tweets with a location tag
+		QueryLocations world = new QueryLocations( new Coordinates( -180f, -90f ), new Coordinates( 180f, 90f ) );
+		access.AddQueryParameter( world );
 		
-			// Get all tweets with a location tag
-			QueryLocations world = new QueryLocations( new Coordinates( -180f, -90f ), new Coordinates( 180f, 90f ) );
-			access.AddQueryParameter( world );
-			
-			// and with the word "bieber" in them
-			access.AddQueryParameter( new QueryTrack( "bieber" ) );
-			
-			access.Connect ( false );
-			
-			while ( true )
+		// and with the word "bieber" in them
+		access.AddQueryParameter( new QueryTrack( "bieber" ) );
+		
+		access.Connect ( false );
+		
+		while ( true )
+		{
+			if ( access.tweets.Count > 0 )
 			{
-				if ( access.tweets.Count > 0 )
-				{
-					Tweet newTweet = access.tweets.Dequeue();
-					Console.WriteLine( newTweet.userName  + ": " + newTweet.status );
-				}
+				Tweet newTweet = access.tweets.Dequeue();
+				Console.WriteLine( newTweet.userName  + ": " + newTweet.status );
 			}
-			
-			// SHOULD ALWAYS CALL DISCONNECT on access object. THIS DOESN"T!!!
 		}
+		
+		// SHOULD ALWAYS CALL DISCONNECT on access object. THIS DOESN"T!!!
 	}
 }
 
